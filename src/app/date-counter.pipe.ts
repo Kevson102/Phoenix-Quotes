138509af4efb4908710e4253dd5d91a3d2daec81
Kevson102 <nyambura.njuguna@student.moringaschool.com>
2021-10-11 19:11:58 +0300

    EDITS THE VARIOUS STYLESHEETS TO ALIGN THE PAGE WITH THE DESIGN

--- src/app/date-counter.pipe.ts
+++ src/app/date-counter.pipe.ts
@@ -9,25 +9,31 @@ export class DateCounterPipe implements PipeTransform {
     let timeElapsed: string = "";
     let currentDate: any = new Date();
     var dateDifference = Math.abs(entry - currentDate) / 1000;
+    
     var NoOfDays = Math.floor(dateDifference / 86400);
     if(NoOfDays > 0){
-      timeElapsed   += Math.floor(NoOfDays) +" days ";
+      timeElapsed += Math.floor(NoOfDays) +" days ";
     }
+
     dateDifference -= NoOfDays * 86400;
+
     var NoOfHours = Math.floor(dateDifference / 3600) % 24;
     if(NoOfHours > 0){
       timeElapsed += Math.floor(NoOfHours)+" hrs ";
     }
+
     dateDifference -= NoOfHours * 3600;
 
     var NoOfMinutes = Math.floor(dateDifference / 60) % 60;
     if(NoOfMinutes > 0){
       timeElapsed += Math.floor(NoOfMinutes)+" mins ";
     }
+
     dateDifference -= NoOfMinutes * 60;
-    var NoOfSeconds    = dateDifference % 60;
+
+    var NoOfSeconds = dateDifference % 60;
     if(NoOfSeconds > 0){
-      timeElapsed  += Math.floor(NoOfSeconds)+" secs ";
+      timeElapsed += Math.floor(NoOfSeconds)+" secs ";
     }
     return timeElapsed;
   }
